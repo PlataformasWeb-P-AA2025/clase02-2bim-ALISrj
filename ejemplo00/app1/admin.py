6ee88a2fa1bc4421cbc4f05b97a36d8d816968ea
Alex R <alexis.rj2110@gmail.com>
2025-06-11 08:48:28 -0500

    V0.0.3

--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Estudiante, Ciudad
+
+admin.site.register(Estudiante)
+admin.site.register(Ciudad)
--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    cedula = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return "%s - %s - %s - edad: %d" % (self.nombre,
+                                            self.apellido,
+                                            self.cedula,
+                                            self.edad)
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+    poblacion = models.IntegerField()
+
+    def __str__(self):
+        return "%s - %s" % (self.nombre, self.poblacion)
